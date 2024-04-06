d57e656bd08b0da8aee04f13a88279769a4192cd
Ben Cunningham <30294640+SummerCourse17@users.noreply.github.com>
2024-04-06 18:23:16 +0100

    Adding Basic Layout components

---
+++ site/podcast.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BC Podcasts</title>
+        <link rel="stylesheet" href="./assets/style.css">
+    </head>
+    <body>
+        <header>
+            <h2>Podcasts</h2>
+            <nav>
+                <p>Admin Login</p>
+            </nav>
+        </header>
+        <main>
+            <h1>Podcast Name</h1>
+            <p>Podcast Author</p>
+            <p>Podcast Description</p>
+        </main>
+        <div class="pod_box">
+            <div class="podcast">
+                <div class="pod_image">
+                    <img src="https://bcwd.site/bc-brands-trans.png">
+                </div>
+                <div class="pod_info">
+                    <h2><a href="">Episode Name</a></h2>
+                    <p>Episode Description</p>
+                    <audio controls>
+
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>Powered by <a href="https://github.com/BC-Brands/BC-Podcasts-v2/">BC Podcasts</a></p>
+        </footer>
+    </body>
+</html>
